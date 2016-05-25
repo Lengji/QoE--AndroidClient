397b6806804f9f2f5d6698c0ac664ee94c2e720f
LENGJI <lengjireji@outlook.com>
2016-05-25 15:14:18 +0800

    全屏修复
    修复了全屏时仍可以滑动的Bug

--- qoeclient/src/main/java/com/qoe/lengji/qoeclient/PlayerActivity.java
+++ qoeclient/src/main/java/com/qoe/lengji/qoeclient/PlayerActivity.java
@@ -5,15 +5,21 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class PlayerActivity extends Activity implements CustomVideoView.canFullScreen {
 
     private CustomVideoView mVideoView;
+    private TextView textForScore;
     private RatingBar mRatingBar;
+    private TextView titleView;
+    private TextView descrView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,11 @@ public class PlayerActivity extends Activity implements CustomVideoView.canFullS
     }
 
     private void initVideoView() {
+        titleView = (TextView) findViewById(R.id.video_title);
+        descrView = (TextView) findViewById(R.id.video_description);
+        textForScore = (TextView) findViewById(R.id.text_score);
+        mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+
         mVideoView = (CustomVideoView) findViewById(R.id.videoview);
         mVideoView.setParentView(findViewById(R.id.videoviewContainer));
         mVideoView.setMediaController(new CustomMediaController(this));
@@ -44,31 +55,38 @@ public class PlayerActivity extends Activity implements CustomVideoView.canFullS
     private void playVideo() {
         Intent intent = getIntent();
         Video video = (Video) intent.getSerializableExtra("video");
-        mVideoView.setVideo(video);
-        TextView titleView = (TextView) findViewById(R.id.video_title);
         titleView.setText(video.getTitle());
-        TextView descrView = (TextView) findViewById(R.id.video_description);
         descrView.setText(video.getDescription());
-        mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+        mVideoView.setVideo(video);
         mVideoView.requestFocus();
     }
 
     @Override
     public boolean makeFullScreen() {
+        titleView.setVisibility(View.GONE);
+        descrView.setVisibility(View.GONE);
+        textForScore.setVisibility(View.GONE);
+        mRatingBar.setVisibility(View.GONE);
+
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        getWindow().setAttributes(params);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setAttributes(params);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         return true;
     }
 
     @Override
     public boolean exitFullScreen() {
+        titleView.setVisibility(View.VISIBLE);
+        descrView.setVisibility(View.VISIBLE);
+        mRatingBar.setVisibility(View.VISIBLE);
+        textForScore.setVisibility(View.VISIBLE);
+
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setAttributes(params);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setAttributes(params);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         return true;
     }
